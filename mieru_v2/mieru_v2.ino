77fe981074a989c9727c2f95c45342b8e7a996a2
yuiswi <r.newbold.yuisuke620@gmail.com>
2025-08-20 09:41:02 +0900

    v2のファイルを追加しました

---
+++ mieru_v2/mieru_v2.ino
@@ -0,0 +1,32 @@
+/* 
+
+  Sketch Title:
+  mieruプロジェクトのバージョン更新版
+  Ver1との変更点
+  * LINE自動通知のAPIとしてLINENotifyを採用していたが、サービス終了したため、公式が推奨しているMessagingAPIに切り替える
+  * ターゲットデバイスをM5Capsuleに変更
+
+	The circuit:
+  * ターゲットデバイス：M5Capsuleに変更（Ver1ではM5StickC Plusを採用）
+	* 土壌水分測定センサ
+  * ポンプ（給水用）
+
+	Created 2025年8月20日
+	By Yusuke Oike
+	Modified 年月日
+	By 
+
+*/
+
+// 以前開発したLINE自動通知機能について
+// LINENotifyがサービス終了したため、MessagingAPIに切り替えて運用するための更新バージョン
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
